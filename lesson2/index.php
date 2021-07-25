9a1e3076f373cd7eb723af7a18620fbdc4092d81
Галина Михайлова <galina89ruzhyk@mail.ru>
2021-07-25 20:11:07 +0800

    Delete bitrix files

--- lesson2/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
-
-$APPLICATION->includeComponent(
-    'ylab:element.list',
-    'grid',
-);
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
--- local/components/ylab/element.list/templates/.default/lang/ru/template.php
+++
@@ -1,6 +0,0 @@
-<?php
-$MESS['YLAB.ELEMENT.LIST.TITLE'] = 'Товар:';
-$MESS['YLAB.ELEMENT.LIST.PRICE'] = 'Закупочная цена: ';
-$MESS['YLAB.ELEMENT.LIST.PERCENT'] = 'Наценка(%): ';
-$MESS['YLAB.ELEMENT.LIST.TOTAL'] = 'Розничная цена: ';
-$MESS['YLAB.ELEMENT.LIST.STATUS'] = 'Статус: ';
